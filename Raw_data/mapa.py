35d5ccba04f96a27264414f191c7d92a858451eb
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2019-02-26 14:04:46 -0300

    Adiocona os scripts para os dados brutos

---
+++ Raw_data/mapa.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 13:32:27 2019
+
+@author: Usuário
+"""
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
+import pandas as pd
+
+npzfile = np.load('prec_raw.npz')
+latlon = npzfile['latlon']
+df_latlon = pd.DataFrame(latlon) 
+
+mapbox_access_token = 'pk.eyJ1IjoibHVjYXN1Y2hvYSIsImEiOiJjanFxdjVjNjMwZjcwM3htdm9pbzViOW4wIn0.E8WOGX1mAGUjr0O4fbcXVA'
+
+data = [
+    go.Scattermapbox(
+        lat=df_latlon[0].tolist(),
+        lon=df_latlon[1].tolist(),
+        mode='markers',
+        marker=dict(size=9),
+    )
+]
+
+layout = go.Layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=-10.116700,
+            lon=-36.650002
+        ),
+        pitch=0,
+        zoom=10
+    ),
+)
+
+fig = dict(data=data, layout=layout)
+py.iplot(fig, filename='1212 Multiple Mapbox')
---
+++ Raw_data/scritp.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  8 15:06:40 2019
+
+@author: Usuário
+"""
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
+import pandas as pd
+
+
+npzfile = np.load('prec_raw.npz')
+#estou enfrentando problemas com memória na proxima linha
+var = npzfile['var']
+latlon = npzfile['latlon']
+days = npzfile['days']
+
+
+#modo não muito eficiente de se tratar dados de dias
+df_var = pd.DataFrame(var).T
+#outro jeito de ter a serie de dias do df_days, achei melhor pois tem mais conteudo desse formato na web
+nday = pd.date_range('1/1/1980',periods=13149,freq='D')
+df_latlon = pd.DataFrame(latlon) 
+df_days = pd.DataFrame(days)
+
+
+
+data = [go.Scatter(x=nday, y=df_var[0])]
+
+py.iplot(data, filename = 'Ruberto_var')
