784e84fa5dd2e116e32895c5aac6ddfb52e342c0
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2019-03-12 16:54:33 -0300

    Estrutura dos codigos

---
+++ Estrutura_de_codigo.py
@@ -0,0 +1,53 @@
+def ler_dados(dado: np.):
+    """
+    Essa função lê os dados disponiveis no link
+    https://utexas.app.box.com/v/Xavier-etal-IJOC-DATA
+    
+    A função recebe dados (no formato .nc, .npc, etc) e retorna um xarray dele
+    
+    Lucas e Mateus
+    """
+    return dado_da_grade
+
+def faz_mapa(dado_da_grade: xr.DataArray):
+    """
+    Essa função faz um mapa com o recorte do brasil
+    
+    A função recebe os dados da grade e retorna um mapa
+   
+    Mateus
+    """
+    return mapa
+
+def faz_regiao_hidrografica(dados_shp: str):
+    """
+    Essa função faz o poligono das regiões hidrograficas
+    
+    Essa função recebe os dados da região hidrográfica do estado de alagoas e 
+    retorna o poligono selecionado
+    
+    Lucas
+    """
+    return poligono
+
+def intersecta_dado(dados_da_grade: xr.DataArray, poligono: gpd.GeoDataFrame):
+    """
+    Essa função produz o poligono interseção de dois recortes
+    
+    A função recebe os dados da grade e o poligono da região hidrográfica e 
+    retorna a interseção de ambos em xr.DataArray
+    
+    Micaellen e Lucas
+    """
+    return interseccao
+    
+def faz_grafico(interseccao: xr.DataArray):
+    """
+    Essa função produz gráficos estatisticos
+    
+    A função recebe dados da grade e retorna um gráfico
+    
+    Micaellen e Mateus
+    """
+    
+    return grafico
